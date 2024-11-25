18884806750b93c32c6b8499693008fcf2d176ce
tenesedu <tenes.trillo.eduardo@gmail.com>
2024-11-25 21:49:58 +0100

    <p> before <form> fix

--- components/form/metadata.tsx
+++ components/form/metadata.tsx
@@ -32,74 +32,76 @@ const MetadataForm = ({ onProcessMetadata }: MetadataFormProps) => {
   };
 
   return (
-    <form className="p-4 mx-auto">
-      <div className="mb-4 flex flex-col lg:flex-row gap-6">
-        <div className="items-center gap-2">
-          <label
-            className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
-            htmlFor="title"
-          >
-            Title:
-          </label>
-          <input
-            value={metadata.title}
-            className="block w-full border border-gray-300 rounded-md p-2"
-            type="text"
-            id="title"
-            onChange={handleChange}
-          />
+    <div>
+      <form className="p-4 mx-auto">
+        <div className="mb-4 flex flex-col lg:flex-row gap-6">
+          <div className="items-center gap-2">
+            <label
+              className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
+              htmlFor="title"
+            >
+              Title:
+            </label>
+            <input
+              value={metadata.title}
+              className="block w-full border border-gray-300 rounded-md p-2"
+              type="text"
+              id="title"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="items-center gap-2">
+            <label
+              className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
+              htmlFor="author"
+            >
+              Author:
+            </label>
+            <input
+              value={metadata.author}
+              className="block w-full border border-gray-300 rounded-md p-2"
+              type="text"
+              id="author"
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        <div className="items-center gap-2">
-          <label
-            className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
-            htmlFor="author"
-          >
-            Author:
-          </label>
-          <input
-            value={metadata.author}
-            className="block w-full border border-gray-300 rounded-md p-2"
-            type="text"
-            id="author"
-            onChange={handleChange}
-          />
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="items-center gap-2">
+            <label
+              className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
+              htmlFor="genre"
+            >
+              Genre:
+            </label>
+            <input
+              value={metadata.author}
+              className="block w-full border border-gray-300 rounded-md p-2"
+              type="text"
+              id="genre"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4 items-center gap-2">
+            <label
+              className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
+              htmlFor="language"
+            >
+              Language:
+            </label>
+            <input
+              value={metadata.language}
+              className="block w-full border border-gray-300 rounded-md p-2"
+              type="text"
+              id="language"
+              onChange={(e) =>
+                setMetadata({ ...metadata, language: e.target.value })
+              }
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col lg:flex-row gap-6">
-        <div className="items-center gap-2">
-          <label
-            className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
-            htmlFor="genre"
-          >
-            Genre:
-          </label>
-          <input
-            value={metadata.author}
-            className="block w-full border border-gray-300 rounded-md p-2"
-            type="text"
-            id="genre"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4 items-center gap-2">
-          <label
-            className="block text-sm font-medium text-gray-700 min-w-[65px] mb-1"
-            htmlFor="language"
-          >
-            Language:
-          </label>
-          <input
-            value={metadata.language}
-            className="block w-full border border-gray-300 rounded-md p-2"
-            type="text"
-            id="language"
-            onChange={(e) =>
-              setMetadata({ ...metadata, language: e.target.value })
-            }
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
